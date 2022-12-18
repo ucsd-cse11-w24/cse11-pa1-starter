d3fdc45648c386071e16759a4d2942277c6c7784
yeoheeim-ucsd <120440576+yeoheeim-ucsd@users.noreply.github.com>
2022-12-17 16:32:49 -0800

    Create CompileTimeErrors.java
    Part 2 of PA 1

---
+++ CompileTimeErrors.java
@@ -0,0 +1,16 @@
+import tester.*;
+
+class CompileTimeErrors{
+    
+    // calculate the area of a right triangle
+    double rightTriangleArea(double base, double height){
+        return base * height / 2
+    }
+    
+    // square a number
+    int squareNumber(int myNumber){
+        return myNumber * myNumer;
+    }
+  
+    int example = this.rightTriangeArea(3, 5); // expected 7.5
+    String fiveSquared = this.squareNumber(5); // expected 25
